f5167c9d7314f904fc2e18c6fd597fe2681b4af9
jesusrlv <jesusrlvrojo@gmail.com>
2024-06-28 16:33:00 -0600

    listo 2

--- contact.php
+++ contact.php
@@ -1,17 +1,17 @@
 <?php
 	if(!isset( $_POST['name']) || !isset($_POST['email']) || !isset($_POST['message']) ) {
-		echo 'something is wrong! Please try agian';
+		echo 'error, intenta nuevamente';
 		die();
 	}
 	$email_from = $_POST['email'];
-	$email_message = "Please find below a message submitted by ".stripslashes($_POST['name'])."\n\n";
-	$email_message .=" on ".date("d/m/Y")." at ".date("H:i")."\n\n";
+	$email_message = "Revisar correo de ".stripslashes($_POST['name'])."\n\n";
+	$email_message .=" en fecha ".date("d/m/Y")." at ".date("H:i")."\n\n";
 	$email_message .= stripslashes($_POST['message']);
 
-	$headers = 'From: '.$email_from."\r\n" .
+	$headers = 'De: '.$email_from."\r\n" .
    'Reply-To: '.$email_from."\r\n" ;
 
-	mail('abuubaidahtamim@gmail.com', $email_subject, $email_message, $headers);
-	echo "Thanks for contacting us.";
+	mail('jesusrlvrojo@gmail.com', $email_subject, $email_message, $headers);
+	echo "Gracias por contactarnos.";
 	die();
 ?>
